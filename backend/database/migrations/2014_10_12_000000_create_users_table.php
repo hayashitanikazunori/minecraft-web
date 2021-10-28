c04767d86c73d79acad66d6e89188614c5176547
hayashitanikazunori <fworkkaz@gmail.com>
2021-10-28 22:35:08 +0900

    マイグレーションファイルの作成

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('thumbnail_images')->comment('サムネイル画像');
+            $table->string('profile')->comment('プロフィール本文');
+            $table->string('freezing_status', 3)->comment('凍結ステータス');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ backend/database/migrations/2021_10_28_193521_create_favorites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->comment('ユーザーID');;
+            $table->foreignId('post_id')->constrained()->onDelete('cascade')->comment('投稿ID');;
+            $table->timestamps();
+
+            // user_idとpost_idの組み合わせの重複を許さない
+            $table->unique(['user_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
