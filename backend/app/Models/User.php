a32e983adc76f993b259c0c912e14f55dbf06c70
hayashitanikazunori <fworkkaz@gmail.com>
2021-11-10 04:27:49 +0900

    バリデーションとfillableの設定

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar_image',
+        'profile',
+        'freezing_status',
     ];
 
     /**
