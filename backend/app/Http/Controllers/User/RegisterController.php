0d8f9ecd9ce7b24ce26164f85d38eb480e275c77
hayashitanikazunori <fworkkaz@gmail.com>
2021-11-16 16:35:49 +0900

    画像アップロード実装完了

--- backend/app/Http/Controllers/User/RegisterController.php
+++ backend/app/Http/Controllers/User/RegisterController.php
@@ -12,15 +12,24 @@ use Exception;
 class RegisterController extends Controller
 {
     public function register(UserRegisterRequest $request): JsonResponse {
-
+    /*************************************************
+     * TODO
+     * $jsonの'data' => $userについては頃合いを見て削除すること。
+     * リファクタリングをしたい。
+     * imgaesの参照先は下記のファイルを参考にすること。
+     * https://qiita.com/koru1893/items/1d2f522e20744b03e3ad
+     * https://qiita.com/___yusuke49/items/9f6f64c7f800b8e77e7d
+    *************************************************/
         try {
                 $request->validated();
+
+                $image_path = $request->avatar_image->store('public/avatar/');
 
                 $user = User::create([
                     'name' => $request->name,
                     'email' => $request->email,
                     'password' => Hash::make($request->password),
-                    'avatar_image' => $request->avatar_image,
+                    'avatar_image' => basename($image_path),
                     'profile' => $request->profile,
                     'freezing_status' => 0,
                 ]);
