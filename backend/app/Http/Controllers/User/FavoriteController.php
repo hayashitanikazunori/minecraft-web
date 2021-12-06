4e54035412100a2d73b9285923d5850733390330
hayashitanikazunori <fworkkaz@gmail.com>
2021-12-06 20:27:09 +0900

    controller ジェネレート

---
+++ backend/app/Http/Controllers/User/FavoriteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FavoriteController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
