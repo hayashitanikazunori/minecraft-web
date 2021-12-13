b30c0097ae21832b161538ef06c10cb6c65e77ab
hayashitanikazunori <fworkkaz@gmail.com>
2021-12-13 20:20:39 +0900

    user リファクタリング完了

--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use \App\Http\Requests\UserRegisterRequest;
-use \App\Http\Requests\UserCreateRequest;
 use App\Models\User;
 use App\Models\Post;
-use App\Http\Resources\User\UserResource;
+use Illuminate\Http\Request;
+use \App\Http\Requests\UserCreateRequest;
+use \App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\User\PostsCollection;
+use App\Http\Resources\User\UserResource;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Hash;
 use Exception;
 
 class UserController extends Controller
@@ -58,37 +57,16 @@ class UserController extends Controller
         }
     }
 
-    public function update(UserRegisterRequest $request, $id): JsonResponse
+    public function update(UserUpdateRequest $request, $id): JsonResponse
     {
-        /*************************************************
-         * TODO
-         * リファクタリングをしたい。
-         * DBとのやりとりはModelに移したい。
-         * formRequestがregisterを使っているので、update用のformRequestを作ること。
-         * imgaesの参照先は下記のファイルを参考にすること。
-         * https://qiita.com/koru1893/items/1d2f522e20744b03e3ad
-         * https://qiita.com/___yusuke49/items/9f6f64c7f800b8e77e7d
-        *************************************************/
         try {
-            $request->validated();
-            $image_path = $request->avatar_image->store('public/avatar/');
+            $credentials = $request->validated();
 
-            $user = User::find($id);
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->password = Hash::make($request->password);
-            $user->avatar_image = basename($image_path);
-            $user->profile = $request->profile;
-            $user->save();
+            $user = new User;
+            $user = $user->userUpdate($credentials, $id);
 
-            $json = [
-                'userUpdate' => $user,
-                'message' => '変更に成功しました。',
-            ];
-
-            return new JsonResponse($json);
-
-            } catch (Exception $e) {
+            return new JsonResponse(['message' => '変更に成功しました。' ]);
+        } catch (Exception $e) {
             return new JsonResponse([ 'message' => '変更に失敗しました。再度お試しください。', 'errorMessage' => $e]);
         }
     }
@@ -96,8 +74,8 @@ class UserController extends Controller
     public function destroy($id): JsonResponse
     {
         try {
-            $user = User::find($id);
-            $user->delete();
+            $user = new User;
+            $user->userDelete($id);
 
             return new JsonResponse(['message' => 'アカウントの削除に成功しました。' ]);
         } catch (Exception $e){
